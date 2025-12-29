d178e8d1e4bb26dc1ef6d6b7a9c579e5edd66d2f
Ayse <mutluayayse230@gmail.com>
2025-12-29 22:47:38 +0300

    proje dosyaları eklendi

---
+++ VirusSimulasyonu/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace VirusSimulasyonu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVirus = new System.Windows.Forms.Button();
+            this.btnAntivirus = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVirus
+            // 
+            this.btnVirus.Location = new System.Drawing.Point(56, 113);
+            this.btnVirus.Name = "btnVirus";
+            this.btnVirus.Size = new System.Drawing.Size(231, 57);
+            this.btnVirus.TabIndex = 0;
+            this.btnVirus.Text = "VİRÜS";
+            this.btnVirus.UseVisualStyleBackColor = true;
+            this.btnVirus.Click += new System.EventHandler(this.btnVirus_Click);
+            // 
+            // btnAntivirus
+            // 
+            this.btnAntivirus.Location = new System.Drawing.Point(56, 219);
+            this.btnAntivirus.Name = "btnAntivirus";
+            this.btnAntivirus.Size = new System.Drawing.Size(231, 63);
+            this.btnAntivirus.TabIndex = 1;
+            this.btnAntivirus.Text = "ANTİVİRÜS";
+            this.btnAntivirus.UseVisualStyleBackColor = true;
+            this.btnAntivirus.Click += new System.EventHandler(this.btnAntivirus_Click);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.ItemHeight = 16;
+            this.lstLog.Location = new System.Drawing.Point(432, 58);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(308, 276);
+            this.lstLog.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.btnAntivirus);
+            this.Controls.Add(this.btnVirus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVirus;
+        private System.Windows.Forms.Button btnAntivirus;
+        private System.Windows.Forms.ListBox lstLog;
+    }
+}
+
---
+++ VirusSimulasyonu/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VirusSimulasyonu // Proje adın farklıysa burayı düzeltmeyi unutma!
+{
+    // KURAL: Form1 sınıfı her zaman en üstte olmalı
+    public partial class Form1 : Form
+    {
+        string testKlasoru = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "OdevTest");
+
+        public Form1()
+        {
+            InitializeComponent();
+            if (!Directory.Exists(testKlasoru)) Directory.CreateDirectory(testKlasoru);
+        }
+
+        private void btnVirus_Click(object sender, EventArgs e)
+        {
+            IProgram virus = new ZararliYazilim();
+            virus.Calistir(testKlasoru, lstLog);
+        }
+
+        private void btnAntivirus_Click(object sender, EventArgs e)
+        {
+            IProgram antivirus = new AntivirusYazilimi();
+            antivirus.Calistir(testKlasoru, lstLog);
+        }
+    }
+
+    // DİĞER SINIFLAR VE INTERFACE FORMUN ALTINDA OLMALI
+    public interface IProgram
+    {
+        void Calistir(string yol, ListBox liste);
+    }
+
+    public class ZararliYazilim : IProgram
+    {
+        public void Calistir(string yol, ListBox liste)
+        {
+            liste.Items.Add("Virüs aktifleşti...");
+            for (int i = 1; i <= 5; i++)
+            {
+                string dosyaAdi = $"tehdit_{i}.txt";
+                string tamYol = Path.Combine(yol, dosyaAdi);
+                File.WriteAllText(tamYol, "VİRÜS_İMZASI_X123");
+                liste.Items.Add(dosyaAdi + " oluşturuldu.");
+            }
+        }
+    }
+
+    public class AntivirusYazilimi : IProgram
+    {
+        public void Calistir(string yol, ListBox liste)
+        {
+            liste.Items.Add("Tarama başlıyor...");
+            string[] dosyalar = Directory.GetFiles(yol);
+            int temizlenen = 0;
+            foreach (string dosya in dosyalar)
+            {
+                if (File.ReadAllText(dosya).Contains("VİRÜS_İMZASI_X123"))
+                {
+                    File.Delete(dosya);
+                    liste.Items.Add("SİLİNDİ: " + Path.GetFileName(dosya));
+                    temizlenen++;
+                }
+            }
+            liste.Items.Add($"{temizlenen} tehdit yok edildi.");
+        }
+    }
+}
